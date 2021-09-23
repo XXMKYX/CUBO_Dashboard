19ac75a1d5a00bef73ed91c7b4caca296da14075
Miguel Angel <51009414+XXMKYX@users.noreply.github.com>
2021-09-23 15:42:51 -0500

    Dashboard proyect
    Nodejs // React // Chartjs

---
+++ dashboard/src/cpnt/dash.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Bar, Line, Pie, Doughnut } from 'react-chartjs-2';
+
+class dash extends Component {
+    render() { 
+        return (
+        <div className="dash">
+            Dashboard
+        </div>
+        )
+    }
+}
+ 
+export default dash ;
